6fdcf2dcb1c8e4b99d268e28adc297db72dbe748
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-05-25 13:47:23 +0200

    Change fetal measurement test elements paths

--- model/test_fetal_measurement.py
+++ model/test_fetal_measurement.py
@@ -6,13 +6,13 @@ from model.fetal_measurement import FetalMeasurement
 
 
 @pytest.mark.parametrize('image_path, prediction_path, body_part', [
-    ('../data/samples/images/0427_abdomen_80.png', '../data/samples/predictions/0427_abdomen_80.png', 'abdomen'),
-    ('../data/samples/images/0427_femur_43.png', '../data/samples/predictions/0427_femur_43.png', 'femur'),
-    ('../data/samples/images/0427_head_203.png', '../data/samples/predictions/0427_head_203.png', 'head'),
-    ('../data/samples/images/no_medical_image.png', '../data/samples/predictions/no_medical_image.png', 'unclassified'),
+    ('data/samples/images/0427_abdomen_80.png', 'data/samples/predictions/0427_abdomen_80.png', 'abdomen'),
+    ('data/samples/images/0427_femur_43.png', 'data/samples/predictions/0427_femur_43.png', 'femur'),
+    ('data/samples/images/0427_head_203.png', 'data/samples/predictions/0427_head_203.png', 'head'),
+    ('data/samples/images/no_medical_image.png', 'data/samples/predictions/no_medical_image.png', 'unclassified'),
 ])
 def test_predictions(image_path, prediction_path, body_part):
-    measurer = FetalMeasurement(model_path='trained/weights.pt')
+    measurer = FetalMeasurement(model_path='model/trained/weights.pt')
     # Maximum bits that could be different between the hashes.
     cutoff = 3
     # Start of the test.
